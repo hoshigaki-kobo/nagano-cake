69b151dfe8c79ce7a7aa9c83c6916df9da747219
EC2 Default User <ec2-user@ip-172-31-6-222.ec2.internal>
2022-02-22 09:12:01 +0000

    [Update]5

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,6 +1,6 @@
 class Public::CustomersController < ApplicationController
 
-  before_action :authenticate_customer!
+  #before_action :authenticate_customer!
 
   def show
     @customer = Customer.find(params:[id])
