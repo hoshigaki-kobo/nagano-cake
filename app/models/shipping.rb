984fd3d3d932b9d5ace151eb4b9ef068bffc737f
RyutoMotohashi <95869388+ryutomotohashi@users.noreply.github.com>
2022-02-23 09:37:35 +0000

    [add]tweak1 items&shippings validate

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -2,6 +2,12 @@ class Shipping < ApplicationRecord
 
   belongs_to :customer
 
+  with_options presence: true do
+    validates :zip_code
+    validates :address
+    validates :name
+  end
+
   def shipping_display
     '〒' + zip_code + ' ' + address + ' ' + name
   end
