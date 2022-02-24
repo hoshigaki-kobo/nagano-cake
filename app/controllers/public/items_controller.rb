eb2c0b5e7e80a31d18ce01cfb9ab31e44722c093
EC2 Default User <ec2-user@ip-172-31-6-222.ec2.internal>
2022-02-24 12:27:17 +0000

    [検索窓]

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -7,10 +7,12 @@ class Public::ItemsController < ApplicationController
   def show
     # 『カートに入れる』form_with用インスタンスの定義
     @item = Item.find(params[:id])
-    if current_customer.cart_items.find_by(item_id:params[:id]).present?
-      @cart_item = current_customer.cart_items.find_by(item_id:params[:id])
-    else
-      @cart_item = CartItem.new
-    end
+     if customer_signed_in?
+      if current_customer.cart_items.find_by(item_id:params[:id]).present?
+        @cart_item = current_customer.cart_items.find_by(item_id:params[:id])
+      else
+        @cart_item = CartItem.new
+      end
+     end
   end
 end
