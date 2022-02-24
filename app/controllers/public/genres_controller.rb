fd2121dfe88fcf12c90dac0dc1f3f579ece4ccb8
RyutoMotohashi <95869388+ryutomotohashi@users.noreply.github.com>
2022-02-24 15:17:53 +0000

    [add]修正

--- app/controllers/public/genres_controller.rb
+++ app/controllers/public/genres_controller.rb
@@ -3,6 +3,7 @@ class Public::GenresController < ApplicationController
   def show
     @genres = Genre.all
     @genre = Genre.find(params[:id])
+    # しょーたさんここから下書き換えました
     @items = @genre.items.where(is_active: true)
     @items_page = @items.page(params[:page])
 
